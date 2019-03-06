92364aae5ca9b15aeecc7189e6a8c5bb4b41083e
hajdbo <boris.hajduk@gmail.com>
2019-03-06 20:45:44 +0800

    create iot device first test

---
+++ app/createdevice.py
@@ -0,0 +1,106 @@
+import csv
+import io
+
+#from google.cloud import pubsub
+from google.oauth2 import service_account
+from googleapiclient import discovery
+from googleapiclient.errors import HttpError
+
+
+
+class CreateIotDevice(object):
+
+  def __init__(self):
+      pass
+
+
+  def get_client():
+  #def get_client(service_account_json):
+    """Returns an authorized API client by discovering the IoT API and creating
+    a service object using the service account credentials JSON."""
+    api_scopes = ['https://www.googleapis.com/auth/cloud-platform']
+    api_version = 'v1'
+    discovery_api = 'https://cloudiot.googleapis.com/$discovery/rest'
+    service_name = 'cloudiotcore'
+
+    #credentials = service_account.Credentials.from_service_account_file(
+    #        service_account_json)
+    #scoped_credentials = credentials.with_scopes(api_scopes)
+
+    discovery_url = '{}?version={}'.format(
+            discovery_api, api_version)
+
+    return discovery.build(
+            service_name,
+            api_version,
+            discoveryServiceUrl=discovery_url)
+    #return discovery.build(
+    #        service_name,
+    #        api_version,
+    #        discoveryServiceUrl=discovery_url,
+    #        credentials=scoped_credentials)
+
+
+  def create_device(self, device_string):
+    (device_id, public_key) = device_string.split('|')
+    public_key = "-----BEGIN PUBLIC KEY-----\n" + public_key + "\n-----END PUBLIC KEY-----\n"
+
+    #service_account_json=""
+    project_id="altostrat-demo"
+    cloud_region="europe-west1"
+    registry_id="altostrat-iot-power-registry"
+
+    registry_name = 'projects/{}/locations/{}/registries/{}'.format(
+            project_id, cloud_region, registry_id)
+
+    #client = get_client(service_account_json)
+    client = get_client()
+    #with io.open(public_key_file) as f:
+    #    public_key = f.read()
+
+    # Note: You can have multiple credentials associated with a device.
+    device_template = {
+        'id': device_id,
+        'credentials': [{
+            'publicKey': {
+                'format': 'ES256_PEM',
+                'key': public_key
+            }
+        }]
+    }
+
+    devices = client.projects().locations().registries().devices()
+    return devices.create(parent=registry_name, body=device_template).execute()
+
+# POST https://cloudiot.googleapis.com/v1/{parent=projects/*/locations/*/registries/*}/devices
+# parent	string
+# The name of the device registry where this device should be created. For example, projects/example-project/locations/us-central1/registries/my-registry.
+# need cloudiot.devices.create
+
+
+#    return None
+
+
+# google-api-python-client
+
+
+
+
+    #headers = {
+    #        'authorization': 'Bearer {}'.format(jwt_token),
+    #        'content-type': 'application/json',
+    #        'cache-control': 'no-cache'
+    #}
+
+    #basepath = '{}/projects/{}/locations/{}/registries/{}/devices/{}/'
+    #template = basepath + 'config?local_version={}'
+    #config_url = template.format(
+    #    base_url, project_id, cloud_region, registry_id, device_id, version)
+
+    #resp = requests.get(config_url, headers=headers)
+
+    #if (resp.status_code != 200):
+    #    print('Error getting config: {}, retrying'.format(resp.status_code))
+    #    raise AssertionError('Not OK response: {}'.format(resp.status_code))
+
+
--- app/main.py
+++ app/main.py
@@ -16,21 +16,21 @@ import logging
 
 from flask import Flask
 from flask import request
-from airports import Airports
+from createiotdevice import CreateIotDevice
 
 app = Flask(__name__)
-airport_util = Airports()
+iotdevice = CreateIotDevice()
 
-@app.route('/airportName', methods=['GET'])
-def airportName():
-    """Given an airport IATA code, return that airport's name."""
-    iata_code = request.args.get('iataCode')
-    if iata_code is None:
-      return 'No IATA code provided.', 400
-    maybe_name = airport_util.get_airport_by_iata(iata_code)
-    if maybe_name is None:
-      return 'IATA code not found : %s' % iata_code, 400
-    return maybe_name, 200
+@app.route('/addDevice', methods=['GET'])
+def createdevice():
+    devstring = request.args.get('deviceString')
+    if devstring is None:
+      return 'No devstring code provided.', 400
+    maybe = iotdevice.create_device(devstring)
+    if maybe is None:
+      return 'Did not create the device : %s' % devstring, 400
+    return maybe, 200
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
